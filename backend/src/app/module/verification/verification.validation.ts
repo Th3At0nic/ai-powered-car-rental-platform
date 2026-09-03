fab45e71c10c42d685ea55a4da1a074fe2e75c8b
Th3At0nic <islammdrahatul@gmail.com>
2026-09-04 00:29:45 +0600

    remoevd unnecessary module and files and code from backend

--- backend/src/app/module/verification/verification.validation.ts
+++
@@ -1,9 +0,0 @@
-import z from 'zod';
-
-export const diditStartVerificationValidationSchema = z.object({
-  body: z.object({
-    platform: z.enum(['ios', 'android'], {
-      message: 'Platform must be either ios or android',
-    }),
-  }),
-});
