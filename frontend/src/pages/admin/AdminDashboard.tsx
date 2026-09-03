1ddec2687fb9e9348c6038275e4e897882676d38
Th3At0nic <islammdrahatul@gmail.com>
2026-09-03 20:56:48 +0600

    first commit with frontend basecode

---
+++ frontend/src/pages/admin/AdminDashboard.tsx
@@ -0,0 +1,11 @@
+const AdminDashboard = () => {
+  return (
+    <div>
+      <h1 className="text-center text-4xl  text-red-500">
+        This is admin dashboard
+      </h1>
+    </div>
+  );
+};
+
+export default AdminDashboard;
---
+++ frontend/src/pages/user/UserDashboard.tsx
@@ -0,0 +1,9 @@
+const UserDashboard = () => {
+  return (
+    <div>
+      <h1 className="text-center text-4xl p-10 m-10">This is userDashboard</h1>
+    </div>
+  );
+};
+
+export default UserDashboard;
