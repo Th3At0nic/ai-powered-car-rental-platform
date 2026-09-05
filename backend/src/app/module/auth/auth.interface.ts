0963890a08a5b42a15e1e3d7a1297c3641533457
Th3At0nic <islammdrahatul@gmail.com>
2026-09-05 16:35:51 +0600

    replaced thea uth interface with the new one for this porject

--- backend/src/app/module/auth/auth.interface.ts
+++ backend/src/app/module/auth/auth.interface.ts
@@ -1,62 +1,16 @@
+export type TRegisterUser = {
+  fullName: string;
+  email: string;
+  password: string;
+};
+
 export type TLoginUser = {
   email: string;
   password: string;
 };
 
-export type TGoogleToken = {
-  idToken: string;
-  role: 'sender' | 'signer';
-};
-
 export type TUserAuthData = {
+  userId: string;
   userEmail: string;
-  userId: string;
   role: string;
-};
-
-export type TUpdatePasswordAndProfileParams = {
-  fullName?: string;
-  profilePic?: string;
-  oldPassword?: string;
-  newPassword?: string;
-};
-
-// ---------------------------
-
-export type TRegisterWithEmail = {
-  fullName: string;
-  email: string;
-  password: string;
-  role: 'sender' | 'signer';
-};
-
-export type TVerifyOTPParams = {
-  email: string;
-  otp: string;
-};
-
-export type TResendOTPParams = {
-  email: string;
-};
-
-export type TLoginWithEmailParams = {
-  email: string;
-  password: string;
-};
-
-export type TForgetPasswordParams = {
-  email: string;
-};
-
-export type TResetPasswordParams = {
-  userId: string;
-  newPassword: string;
-};
-
-export type TAppleJwtPayload = {
-  sub: string;
-  email?: string;
-  email_verified?: string;
-  is_private_email?: string;
-  // no name after first login
 };
